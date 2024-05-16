f053520f883d4c2c2d8b88230400ff0bfa4b5f72
Grant Cummings <grantcummings4429@gmail.com>
2024-05-16 16:15:21 -0400

    finished lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,40 @@
 class Pet:
-    pass
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+
+    def __init__(self, name, pet_type, owner= None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.add_pet_to_all()
+
+    
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, type_of_pet):
+        if type_of_pet in Pet.PET_TYPES:
+            self._pet_type = type_of_pet
+        else : 
+            raise ValueError("Pet type not in list of valid pet types.")
+        
+    def add_pet_to_all(self):
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("pet must be an instanc eof the Pet class")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key = lambda pet: pet.name)
